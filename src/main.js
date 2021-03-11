6b46b3d27ef30ed288bce8e0b9559593bbab2499
Leonardo Vinicius Sanchez Dias <60626709+leonzv@users.noreply.github.com>
2021-03-11 02:26:18 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper1 = new Swiper('.swiper1', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    slidesPerView: 3,
+    autoplay: {
+        delay: 5000,
+      },
+
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+  });
+
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    autoplay: {
+        delay: 5000,
+      },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
   });
